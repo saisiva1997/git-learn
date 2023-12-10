5644a8bed246d03e88f7e24511f9d5d887a247b3
sai.borigi <sai.borigi@ivycomptech.com>
2023-12-10 20:58:31 +0530

    Adding file to new branch'

---
+++ AdditionofNum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AdditionofNumbers{
+	public static void main(String[] args){
+		int num1=20;
+		int num2=30;
+		int num3=num1+num2;
+		System.out.println(num3);
+	}
+}
